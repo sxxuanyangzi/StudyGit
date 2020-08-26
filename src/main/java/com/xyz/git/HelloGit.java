651aaf8e0a37f1a6802a3d6a98982c8b7c257700
ex_xieke <ex_xieke@axatp.com>
2020-08-26 17:48:47 +0800

    分支branch1在15行添加代码

--- src/main/java/com/xyz/git/HelloGit.java
+++ src/main/java/com/xyz/git/HelloGit.java
@@ -12,6 +12,7 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 		System.out.println("master修改代码!");
 		System.out.println("branch1修改代码!");
+		System.out.println("分支branch1在15行修改代码!!");
 	}
 
 }
