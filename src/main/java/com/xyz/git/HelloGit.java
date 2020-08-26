7be96333950f374b0b75f75725dbdf398d7567ce
ex_xieke <ex_xieke@axatp.com>
2020-08-26 17:28:28 +0800

    创建master主干

---
+++ src/main/java/com/xyz/git/HelloGit.java
@@ -0,0 +1,15 @@
+package com.xyz.git;
+
+/**
+ * Description:演示操作Git示例代码
+ * @author ex_xieke
+ * @date 2020年8月26日
+ */
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+
+}
