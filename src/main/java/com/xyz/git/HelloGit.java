58f762925a60e5f5f451637d560004158ae441b7
ex_xieke <ex_xieke@axatp.com>
2020-08-26 17:36:02 +0800

    branch1分支修改代码

--- src/main/java/com/xyz/git/HelloGit.java
+++ src/main/java/com/xyz/git/HelloGit.java
@@ -11,6 +11,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("master修改代码!");
+		System.out.println("branch1修改代码!");
 	}
 
 }
